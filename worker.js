47c653265c6606c9c37cfe02006f9072e83ac1e9
Noysz Fourfect <126023454+Wskas4@users.noreply.github.com>
2024-08-06 18:18:00 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,10 +3,10 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let yourName = "Iyariv"
+let yourName = "Noysz-id"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-//const พร็อกซีไอพีs = ['194.36.179.237'];
+//const พร็อกซีไอพีs = ['202.10.42.30'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 //let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
